baffce5c52a5dc0fe6cfa0795b34db90e1a0d551
sharmaharshitnone <sharmaharshitnone@gmail.com>
2026-01-23 14:54:45 +0530

    This is version 1.0.0.3

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from GitOps! Version 1.0.2');
+  res.end('Hello from GitOps! Version 1.0.3');
 });
 server.listen(3000, () => console.log('Server ready'));
