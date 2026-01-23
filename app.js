5e2d934033f3b5eb2f1f1a00d0fd2a6c59d67a65
sharmaharshitnone <sharmaharshitnone@gmail.com>
2026-01-23 14:12:44 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from GitOps! Version 1.0');
+});
+server.listen(3000, () => console.log('Server ready'));
