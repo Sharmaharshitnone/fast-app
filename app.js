23ef93ea84447d6cb9df1396009372008da010f8
sharmaharshitnone <sharmaharshitnone@gmail.com>
2026-01-23 14:52:17 +0530

    setup CI/CD pipelines

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from GitOps! Version 1.0.1');
+  res.end('Hello from GitOps! Version 1.0.2');
 });
 server.listen(3000, () => console.log('Server ready'));
